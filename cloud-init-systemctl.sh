647b8be6f300dd7416af0f59f2740a47e846545f
Pr0fChen <122897975+Pr0fChen@users.noreply.github.com>
2025-05-26 16:00:50 +0200

    Update and rename cloud-init.sh to cloud-init-systemctl.sh

--- cloud-init-systemctl.sh
+++ cloud-init-systemctl.sh
@@ -35,23 +35,4 @@ systemctl daemon-reload
 systemctl enable tars
 systemctl start tars
 
-cat << 'EOF' > /root/install_docker.sh
-#!/bin/bash
-
-apt-get install apt-transport-https ca-certificates curl gnupg2 software-properties-common sudo
-curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg | sudo apt-key add -
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") $(lsb_release -cs) stable"
-apt-get update
-apt-get install docker-ce
-curl -L https://github.com/docker/compose/releases/download/1.19.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
-docker-compose --version
-
-curl -fsSL https://get.docker.com/ -o get-docker.sh
-sh get-docker.sh
-EOF
-
-chmod u+x install_docker.sh
-./install_docker.sh
-
 echo "Installation terminée"
